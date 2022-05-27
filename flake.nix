c7f3fd3f1f93e9ba000d861551aefe998d7a61ec
Damien Biasotto <damien.biasotto@gmail.com>
2022-05-27 22:41:32 +1000

    Explicitely set the pkgs to the unstable channel.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Kandoro - When kanban meets Pomodoro.";
 
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
